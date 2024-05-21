7fec51b14e128a0d45dd66f20aec77acd2653754
Степанчева Анна Владимировна <anna.stepancheva@aisa.ru>
2024-05-21 20:19:27 +0300

    save

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,11 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "mytodo")
+@Theme(value = "my-theme", variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,32 +1,73 @@
 package com.example.application.views.main;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 
-@PageTitle("Main")
-@Route(value = "")
-public class MainView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+@Route("")
+public class MainView extends AppLayout {
 
     public MainView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        addClassName("main-view");
+        H1 title = new H1("Лёха, расслабься, это пранк");
+        title.getStyle().setTextAlign(Style.TextAlign.CENTER)
+                .setFontSize("40px");
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        VerticalLayout vlayout = new VerticalLayout();
+        vlayout.setPadding(true);
+        vlayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        vlayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
-        add(name, sayHello);
+        H1 hp = new H1("С Днем Рождения!");
+        hp.getStyle().setTextAlign(Style.TextAlign.CENTER)
+                .setFontSize("30px");
+
+        vlayout.add(hp);
+        vlayout.add(getImage("1"));
+        vlayout.add(getPoem());
+
+        Div content = new Div();
+        content.add(title);
+        content.add(vlayout);
+        setContent(content);
     }
 
+    private Image getImage(String number){
+        StreamResource imageResource = new StreamResource(number + ".jpg", () -> getClass().getResourceAsStream("/photo/" + number +".jpg"));
+        Image img = new Image(imageResource, "1");
+        img.getStyle().setWidth("700px");
+        return img;
+    }
+
+
+    private static VerticalLayout getPoem() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setPadding(true);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        Span span = new Span();
+        span.getElement().setProperty("innerHTML", POEM);
+        span.getStyle().setJustifyContent(Style.JustifyContent.CENTER);
+        span.getStyle().setFontSize("20px");
+        layout.add(span);
+        return layout;
+    }
+
+    private static final String POEM = """
+            Тебе желаю, Алексей,<br>
+            Чтоб жизнь сложилась, как по нотам<br>
+            Чтоб было множество друзей,<br>
+            Шуршали в кошельке банкноты!<br>
+            Удача в руки чтоб плыла,<br>
+            Не заблудилась по дороге,<br>
+            Чтоб только в гору шли дела,<br>
+            И счастья в жизни было много!""";
 }
